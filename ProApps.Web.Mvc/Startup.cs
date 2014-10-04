dc8b092a7d3c89067c27d36be6e65ab6553b964d
alprise <cristian.prisecaru@gmail.com>
2014-10-04 20:56:03 +0200

    initial commit, excluded _SetPasswortPartial and _ChangePassowrdPartial files because of compilation errors on server (apphb)

---
+++ ProApps.Web.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProApps.Web.Mvc.Startup))]
+namespace ProApps.Web.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
